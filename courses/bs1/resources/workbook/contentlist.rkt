a1738b3b7c42e32082b7210d34fb42f032796748
Helio <helio@helio-vaio>
2016-10-05 17:54:13 -0300

    commit inicial

---
+++ courses/bs1/resources/workbook/contentlist.rkt
@@ -0,0 +1,59 @@
+; This file specifies the pages that make up the numbered portion of the 
+; workbook for the course containing this resources/workbook directory.  
+
+; The list should contain names of either .scrbl or .pdf files that 
+; are in the workbook/pages directory.  Each list element must 
+; include either a .scrbl or a .pdf extension.
+
+; The workbook will be generated from these pages in order
+
+(
+ ;;;;; UNIT 1 ;;;;;;;;;
+ ("StudentWorkbook.pdf" "NinjaCat-Dissection")
+ ("StudentWorkbook.pdf" "NinjaCat-Coordinates")
+ ("StudentWorkbook.pdf" "Game-Design")
+ ("StudentWorkbook.pdf" "Circle-of-Evaluation-Practice")
+ ;;;;; UNIT 2 ;;;;;;;;;
+ ("StudentWorkbook.pdf" "Unit-2")
+ ("StudentWorkbook.pdf" "Circle-of-Evaluation-Practice-2")
+ ;;;;; UNIT 3 ;;;;;;;;;
+ ("StudentWorkbook.pdf" "Unit-3")
+ ("StudentWorkbook.pdf" "Fast-Functions")
+ ("StudentWorkbook.pdf" "Fast-Functions")
+ ;;;;; UNIT 4 ;;;;;;;;;
+ ("StudentWorkbook.pdf" "Unit-4")
+ "rocket-height.scrbl"
+ "red-square.scrbl"
+ "yard-area.scrbl"
+ ;;;;; UNIT 5 ;;;;;;;;;
+ ("StudentWorkbook.pdf" "Unit-5")
+ "update-danger.scrbl"
+ "update-target.scrbl"
+ ;;;; UNIT 6 ;;;;;;;;;
+ ("StudentWorkbook.pdf" "Unit-6")
+ "safe-left.scrbl"
+ "safe-right.scrbl"
+ ("StudentWorkbook.pdf" "AND-and-OR")
+ "onscreen.scrbl"
+ ;;;; UNIT 7 ;;;;;;;;;
+ ("StudentWorkbook.pdf" "Unit-7")
+ "cost.scrbl"
+ "update-player.scrbl"
+ ;;;; UNIT 8 ;;;;;;;;;
+ ("StudentWorkbook.pdf" "Unit-8")
+ "line-length.scrbl"
+ ("StudentWorkbook.pdf" "distance-example")
+ "distance.scrbl"
+ "collide.scrbl"
+ ;;;; UNIT 9 ;;;;;;;;;
+ ("StudentWorkbook.pdf" "Unit-9")
+ ("StudentWorkbook.pdf" "Lines")
+ ("StudentWorkbook.pdf" "Feedback")
+ ("StudentWorkbook.pdf" "Feedback")
+ "red-shape.scrbl"
+ ("StudentWorkbook.pdf" "Translating-to-Algebra")
+ ("StudentWorkbook.pdf" "Math-Rocket-Distance")
+ ("StudentWorkbook.pdf" "Math-Rocket-Time")
+ ("StudentWorkbook.pdf" "Math-Rocket-Collision")
+ ("StudentWorkbook.pdf" "Math-Blank-Recipe")
+ )
